76512a7b97461f6927bafaae18167ea0b16d076f
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2025-03-02 05:46:46 -0400

    Se creó nuestro primer server island

--- src/actions/places/get-availability-by-id.action.ts
+++ src/actions/places/get-availability-by-id.action.ts
@@ -7,8 +7,12 @@ export const getAvailabilityById = defineAction({
   handler: async (placeId) => {
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    const isAvailable = Math.random() > 0.5; // true | false
-    const spotsAvailable = isAvailable ? 0 : Math.floor(Math.random() * 10) + 1; // 1-11
+    const isAvailable = Math.random() > 0.6; // true | false
+    const spotsAvailable = !isAvailable
+      ? 0
+      : Math.floor(Math.random() * 10) + 1; // 1-11
+
+    console.log({ isAvailable, spotsAvailable });
 
     let message = "No hay espacios disponibles";
     if (isAvailable) {
